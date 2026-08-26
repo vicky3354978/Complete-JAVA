cf23204b1c853f2c8d5e340e6bc1e1965cb2ffb4
vicky kumar <vk3354978@gmail.com>
2026-08-27 02:54:22 +0530

    adding more details

--- Interface/src/FullAbstraction.java
+++ Interface/src/FullAbstraction.java
@@ -3,7 +3,7 @@ public interface FullAbstraction {
     void invoice();
     //in interface all instance variable is final .............
     //these method are abstract method because interface gives full data abstraction
-    //it is by default abstract method
+    //it is by default public abstract method
     //in interface we cannot extends the class we can implements the class
 
     //in abstract class contain abstract or non abstract method

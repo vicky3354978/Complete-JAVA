8d4382283ef51b2652db328d792d6044166f188b
vicky kumar <vk3354978@gmail.com>
2026-08-28 01:15:26 +0530

    create multiple Threads and learn what is main Thread

--- Multithreading/src/FirstPro.java
+++ Multithreading/src/FirstPro.java
@@ -4,7 +4,7 @@ class MyThread extends Thread
     {
         for(int i=1; i<=10; i++)
         {
-            System.out.println("thread is running :"+i);
+            System.out.println("thread is running :"+Thread.currentThread().getName());
         }
     }
 
@@ -13,8 +13,14 @@ class MyThread extends Thread
 
 public class FirstPro {
     static void main() {
+        //the main Thread is created by jvm and main Thread execute main method....
         System.out.println("hello");
-        MyThread t=new MyThread();
-        t.start();
+        MyThread t1=new MyThread();
+        t1.start();//t1 Thread created by main thread
+        System.out.println(Thread.currentThread().getName());
+        MyThread t2=new MyThread();
+        t2.start();//this Thread also created by main Thread
+        //two Threads are created Thread-0 and Thread-1
+        //the Thread schedular decides which thread used to execute the program....
     }
 }

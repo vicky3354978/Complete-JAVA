3ee1473a7399e8903a57e4fa684b262bc17aa661
vicky kumar <vk3354978@gmail.com>
2026-09-01 11:47:54 +0530

    Thread join prevention is done

--- Multithreading/src/Prevantion/ThreadJoin.java
+++ Multithreading/src/Prevantion/ThreadJoin.java
@@ -20,7 +20,7 @@ public class ThreadJoin {
         //it waits only 10000ms and 100nano sec.
         //wash.join(1000);
         // -->it wait only 1000ms after this time it will run
-        wash.join();//this join waits until run() method is done
+        wash.join();//this join waits until run() method is done.
         //if I want to execute my run method first so we write wash.join() after write this code
         //drying cloths will not execute first ,firstly start the run method of Thread........
         System.out.println("drying cloths ");

c2a1e39c0f7f160ecf2a765f583bb158242d7838
vicky kumar <vk3354978@gmail.com>
2026-09-16 12:38:55 +0530

    some basic program to create array list ...

--- Collection/src/ArrayListBasicProgram.java
+++ Collection/src/ArrayListBasicProgram.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ArrayListBasicProgram {
     static void main() {
@@ -23,5 +25,20 @@ public class ArrayListBasicProgram {
         //if I want to add other name at 3rd index so we use
         name.add(3,"vishal");
         System.out.println(name);
+
+        //if i want to remove element of arraylist so we use remove method
+
+        name.remove(3);
+        System.out.println(name);
+
+
+        //how to convert array or String type of array into list
+
+        String[] str={"c","s","e"};
+        List<String> convertedList=new ArrayList<>(Arrays.asList(str));
+
+        System.out.println(convertedList);
+        convertedList.remove(1);
+        System.out.println(convertedList);
     }
 }

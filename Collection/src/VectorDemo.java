be52a9ec8f24d4ca448d922b4138a675ec439cdf
vicky kumar <vk3354978@gmail.com>
2026-09-19 14:54:52 +0530

    adding some cursor function in the vector class

--- Collection/src/VectorDemo.java
+++ Collection/src/VectorDemo.java
@@ -1,3 +1,5 @@
+import java.util.Enumeration;
+import java.util.Iterator;
 import java.util.Vector;
 
 public class VectorDemo {
@@ -9,6 +11,22 @@ public class VectorDemo {
         vector.add(40);
         vector.add(30);
         vector.remove(3);
-        System.out.println(vector);
-    }
+        //System.out.println(vector);--->iss line main internally loop lagata hai..
+        //without using this line how to print vector element ?
+        //we use enumeration
+        System.out.println("using enumeration ");
+       Enumeration<Integer> enumeration= vector.elements();
+       while(enumeration.hasMoreElements())//-->this condition check collection have element or not
+       {
+           Integer data=enumeration.nextElement();//--->this next element is generic type so it is write like Integer.
+           System.out.println(data);
+       }
+        Iterator <Integer> iterator= vector.iterator();
+        System.out.println("using iterator ");
+       while(iterator.hasNext())
+       {
+           Integer data=iterator.next();
+           System.out.println(data);
+       }
+     }
 }

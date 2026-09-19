2a0e6781b4bc8271c2cf2a4b04644a0f30458dc4
vicky kumar <vk3354978@gmail.com>
2026-09-19 12:40:14 +0530

    how to create queue using Linked list

--- Collection/src/QueueUsingLinkedList.java
+++ Collection/src/QueueUsingLinkedList.java
@@ -6,5 +6,9 @@ public class QueueUsingLinkedList {
     static void main() {
         Queue<Integer> queue=new LinkedList<>(Arrays.asList(1,2,3,4,5,6));
         System.out.println(queue);
+        //queue follows FIFO first in first out
+        //that why when call the remove method the First element is removed
+        queue.remove();
+        System.out.println(queue);
     }
 }
